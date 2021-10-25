d0f9c14dc59644344dc101c6be34cce141882e2a
Shawn Lee <86939788+ShawnLRH@users.noreply.github.com>
2021-10-25 15:39:47 +0800

    updated helper function

--- helper.js
+++ helper.js
@@ -3,11 +3,11 @@ const Mdex = require("./models/mdex");
 const Pancakeswap = require("./models/pancakeswap")
 
 const initExchange = (dexId, exchange) => {
-    if (dexId == 1) {
+    if (dexId == 'pancakeswap') {
         return new Pancakeswap();
-    }else if (dexId == 2) {
+    }else if (dexId == 'kyber') {
         return new Kyber();
-    }else if(dexId == 3) {
+    }else if(dexId == 'mdex') {
         return new Mdex();
     }else{
         console.log("Please add exchange first");
