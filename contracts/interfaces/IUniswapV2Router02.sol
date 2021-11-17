9ca19f84f6804ac8367438e77f5ab2ec4f58e608
Shawn Lee <86939788+ShawnLRH@users.noreply.github.com>
2021-11-17 11:56:51 +0800

    Migrations and truffle config

--- contracts/interfaces/IUniswapV2Router02.sol
+++ contracts/interfaces/IUniswapV2Router02.sol
@@ -1,8 +1,8 @@
 pragma solidity >=0.6.2;
 
-import './IPancakeRouter01.sol';
+import './IUniswapV2Router01.sol';
 
-interface IPancakeRouter02 is IPancakeRouter01 {
+interface IUniswapV2Router02 is IUniswapV2Router01 {
     function removeLiquidityETHSupportingFeeOnTransferTokens(
         address token,
         uint liquidity,
