66bd370fb560b303363d0841e4760d44cae84fbb
Shawn Lee <86939788+ShawnLRH@users.noreply.github.com>
2021-12-06 12:29:29 +0800

    removing excess files

--- migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-const Flashloan = artifacts.require("Arbitrage.sol");
-
-module.exports = function (deployer) {
-  deployer.deploy(
-    Flashloan,
-    '0x6725F303b657a9451d8BA641348b6761A6CC7a17', //PancakeSwap factory
-    '0x3380aE82e39E42Ca34EbEd69aF67fAa0683Bb5c1', //ApeSwap router
-  );
-};
